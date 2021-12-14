52ec2f937337d39002c4171dbebd77c70680a39f
kzeeyang <2402989406@qq.com>
2021-12-14 14:54:32 +0800

    init

--- float.go
+++ float.go
@@ -7,6 +7,6 @@ import (
 
 // DecimalFixed2 - format float or string  with 2 decimal
 func DecimalFixed2(value interface{}) float64 {
-	data, _ := strconv.ParseFloat(fmt.Sprintf("%.2f", value), 64)
+	data, _ := strconv.ParseFloat(fmt.Sprintf("%.2v", value), 64)
 	return data
 }
